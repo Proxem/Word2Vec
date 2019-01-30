0e1913ddc9dc78cda152069a3ef5f9867150b130
thp <thp@proxem.com>
2019-01-30 14:00:15 +0100

    add documentation and classic sort function

--- Proxem.Word2Vec/Sorting.cs
+++ Proxem.Word2Vec/Sorting.cs
@@ -18,11 +18,10 @@
  */
 
 using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 using Proxem.NumNet;
+using Proxem.NumNet.Single;
 
 namespace Proxem.Word2Vec
 {
@@ -58,6 +57,13 @@ namespace Proxem.Word2Vec
             return left;
         }
 
+        /// <summary>
+        /// Heapsort on a single dimensional array.
+        /// Find the k highest values and indices in the given array and return them in the given bestd and bestw
+        /// </summary>
+        /// <param name="scores"> array to sort </param>
+        /// <param name="bestd"> array to store highest values of score </param>
+        /// <param name="bestw"> array to store indices associated with highest values of score </param>
         public static void Heapsort(float[] scores, float[] bestd, int[] bestw)
         {
             var mapping = Enumerable.Range(0, scores.Length).ToArray();
@@ -74,31 +80,52 @@ namespace Proxem.Word2Vec
             }
         }
 
+        /// <summary>
+        /// Parallel version of heapsort for a 2-dimensional array. Heapsort is made on each column in parallel.
+        /// The k highest values and indices of each column are returned in the given bestd and bestw
+        /// </summary>
+        /// <param name="scores"> 2-d array to sort by columns </param>
+        /// <param name="bestd"> array to store the highest values of each column </param>
+        /// <param name="bestw">array to store the indices associated with the highest values of each column </param>
         public static void HeapsortParallel(Array<float> scores, float[][] bestd, int[][] bestw)
         {
             Parallel.ForEach(Enumerable.Range(0, scores.Shape[1]), i => 
             {
-                HeapsortCol(scores, i, bestd, bestw);
+                HeapsortCol(scores, i, bestd[i], bestw[i]);
             });
         }
 
+        /// <summary>
+        /// Heapsort on a 2-dimensional array. heapsort is made on each column.
+        /// The k highest values and indices of each column are returned in the given bestd and bestw
+        /// </summary>
+        /// <param name="scores"> 2-d array to sort by columns </param>
+        /// <param name="bestd"> array to store the highest values of each column </param>
+        /// <param name="bestw">array to store the indices associated with the highest values of each column </param>
         public static void Heapsort(Array<float> scores, float[][] bestd, int[][] bestw)
         {
             for (int i = 0; i < scores.Shape[1]; i++)
             {
-                HeapsortCol(scores, i, bestd, bestw);
+                HeapsortCol(scores, i, bestd[i], bestw[i]);
             }
         }
 
-        private static void HeapsortCol(Array<float> scores, int col, float[][] bestd, int[][] bestw)
+        /// <summary>
+        /// Performs heapsort on the given column of the array.
+        /// </summary>
+        /// <param name="scores"> array to sort </param>
+        /// <param name="col"> column number </param>
+        /// <param name="bestd"> array to store the highest values of each column </param>
+        /// <param name="bestw">array to store the indices associated with the highest values of each column </param>
+        private static void HeapsortCol(Array<float> scores, int col, float[] bestd, int[] bestw)
         {
             var mapping = Enumerable.Range(0, scores.Shape[0]).ToArray();
             Heapify(scores, col, mapping);
             var iter = 0;
-            while (iter < bestd[col].Length)
+            while (iter < bestd.Length)
             {
-                bestw[col][iter] = mapping[0];
-                bestd[col][iter] = (float)scores[0, col];
+                bestw[iter] = mapping[0];
+                bestd[iter] = (float)scores[0, col];
                 scores[0, col] = (float)scores[scores.Shape[0] - iter - 1, col];
                 mapping[0] = mapping[scores.Shape[0] - iter - 1];
                 iter++;
@@ -106,6 +133,12 @@ namespace Proxem.Word2Vec
             }
         }
 
+        /// <summary>
+        /// Simple sort on the given one dimensional array of scores.
+        /// </summary>
+        /// <param name="scores"> array to sort </param>
+        /// <param name="bestd"> array to store highest values of score </param>
+        /// <param name="bestw"> array to store indices associated with highest values of score </param>
         public static void Sort(float[] scores, float[] bestd, int[] bestw)
         {
             var mapping = Enumerable.Range(0, scores.Length).ToArray();
@@ -117,6 +150,42 @@ namespace Proxem.Word2Vec
             }
         }
 
+        public static void Sort(Array<float> scores, float[][] bestd, int[][] bestw)
+        {
+            for (int i = 0; i < scores.Shape[1]; i++)
+            {
+                SortCol(scores, i, bestd[i], bestw[i]);
+            }
+        }
+
+        public static void SortParallel(Array<float> scores, float[][] bestd, int[][] bestw)
+        {
+            Parallel.ForEach(Enumerable.Range(0, scores.Shape[1]), i =>
+            {
+                SortCol(scores, i, bestd[i], bestw[i]);
+            });
+        }
+
+        private static void SortCol(Array<float> scores, int i, float[] bestd, int[] bestw)
+        {
+            //var mapping = Enumerable.Range(0, scores.Shape[0]).ToArray();
+            //Array.Sort(scores[Slicer._, i], mapping);
+            //for (int j = 0; j < bestd.Length; j++)
+            //{
+            //    bestd[j] = (float)scores[j, i];
+            //    bestw[j] = mapping[j];
+            //}
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// performs one split of quicksort given a pivot value and the position of the previous pivot
+        /// </summary>
+        /// <param name="scores"> array to sort </param>
+        /// <param name="mapping"> mapping of indices </param>
+        /// <param name="left"> previous position of the pivot = starting position of current pivoting operation</param>
+        /// <param name="pivotValue"> value of the new pivot </param>
+        /// <returns></returns>
         private static int Partition(float[] scores, int[] mapping, int left, float pivotValue)
         {
             var storeIndex = left;
@@ -136,6 +205,12 @@ namespace Proxem.Word2Vec
             return storeIndex;
         }
 
+        /// <summary>
+        /// Heuristic to find a good new pivot given previous position of the pivot
+        /// </summary>
+        /// <param name="scores"> array of scores to sort </param>
+        /// <param name="left"> previous position of the pivot </param>
+        /// <returns></returns>
         private static float FindPivot(float[] scores, int left)
         {
             // mean value of first + last + random value
@@ -143,6 +218,11 @@ namespace Proxem.Word2Vec
             return 1f / 3 * (scores[left] + scores[centerIndex + left] + scores[scores.Length - 1]);
         }
 
+        /// <summary>
+        /// turns scores into a max-heap, performs the same operations on mapping to keep track of the indices
+        /// </summary>
+        /// <param name="scores"> array to turn into a max-heap </param>
+        /// <param name="mapping"> array of indices to keep track </param>
         private static void Heapify(float[] scores, int[] mapping)
         {
             int count = scores.Length - 1;
@@ -154,6 +234,12 @@ namespace Proxem.Word2Vec
             }
         }
 
+        /// <summary>
+        /// turns the given column of scores into a max-heap, performs the same operations on mapping to keep track of the indices
+        /// </summary>
+        /// <param name="scores"> array to turn into a max-heap </param>
+        /// <param name="i"> column number </param>
+        /// <param name="mapping"> array of indices to keep track </param>
         public static void Heapify(Array<float> scores, int i, int[] mapping)
         {
             int count = scores.Shape[0] - 1;
@@ -165,6 +251,13 @@ namespace Proxem.Word2Vec
             }
         }
 
+        /// <summary>
+        /// siftdown operation for repairing an heap. pops down values that doesn't respect the heap property
+        /// </summary>
+        /// <param name="scores"> heap to repair </param>
+        /// <param name="mapping"> array to keep track of indices </param>
+        /// <param name="start"> starting index to repair </param>
+        /// <param name="end"> end of the heap </param>
         private static void SiftDown(float[] scores, int[] mapping, int start, int end)
         {
             var root = start;
@@ -197,6 +290,14 @@ namespace Proxem.Word2Vec
             }
         }
 
+        /// <summary>
+        /// siftdown operation for repairing an heap. pops down values that doesn't respect the heap property
+        /// </summary>
+        /// <param name="scores"> heap to repair </param>
+        /// <param name="i"> column number </param>
+        /// <param name="mapping"> array to keep track of indices </param>
+        /// <param name="start"> starting index to repair </param>
+        /// <param name="end"> end of the heap </param>
         private static void SiftDown(Array<float> scores, int i, int[] mapping, int start, int end)
         {
             var root = start;
